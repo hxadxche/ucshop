5b724687b74642f0ccf7b72c9e6cca50089769bb
hxadxche <artem.savelev.savelev@gmail.com>
2025-07-19 03:31:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Скачиваем свежую базу данных..."
-curl -o users_orders.db https://raw.githubusercontent.com/ИМЯ/РЕПОЗИТОРИЙ/main/users_orders.db
+curl -o users_orders.db https://raw.githubusercontent.com/hxadxche/ucshop/main/users_orders.db
 
 echo "Запуск Telegram-бота и Webhook-сервера..."
 python3 ucshop.py &  # Бот
