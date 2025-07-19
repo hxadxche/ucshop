d959e51ac50c41f810da011090a77f2f4dac783d
hxadxche <artem.savelev.savelev@gmail.com>
2025-07-19 04:08:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Скачиваем свежую базу данных..."
 curl -o users_orders.db https://raw.githubusercontent.com/hxadxche/ucshop/main/users_orders.db
 
 echo "Запуск Telegram-бота и Webhook-сервера..."
-python3 ucshop.py   # Бот
+python3 ucshop.py &         # Бот
 python3 webhook_server.py  # Flask-сервер
